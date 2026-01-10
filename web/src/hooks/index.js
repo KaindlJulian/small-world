a82e3974a8cf44b4324d5752abbe8451dc0e027e
Julian Kaindl <kaindl745@gmail.com>
2026-01-11 00:24:04 +0100

    Add onclick outside hook

--- web/src/hooks/index.js
+++ web/src/hooks/index.js
@@ -1,3 +1,4 @@
 export { useCardInfo } from './useCardInfo';
 export { useGraphData } from './useGraphData';
+export { useOnClickOutside } from './useOnClickOutside';
 export { useSearcher } from './useSearcher';
---
+++ web/src/hooks/useOnClickOutside.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+/**
+ * @param {Array} refs array of refs to ignore clicks on / decendants of
+ * @param {Function} handler function to call on outside click
+ */
+export function useOnClickOutside(refs, handler) {
+    useEffect(() => {
+        const listener = (event) => {
+            if (
+                refs.some(
+                    (ref) => !ref.current || ref.current.contains(event.target),
+                )
+            ) {
+                return;
+            }
+            handler(event);
+        };
+
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        };
+    }, [...refs, handler]);
+}
