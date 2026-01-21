7edd472a82f9692ed0428c08624d5bcd650f37d1
Julian Kaindl <kaindl745@gmail.com>
2026-01-21 22:46:12 +0100

    decompress gz correctly

--- web/src/api/smallWorld.js
+++ web/src/api/smallWorld.js
@@ -1,15 +1,13 @@
 const csvUrl = 'm.gz';
 
 export async function fetchCsv() {
-    const response = await fetch(csvUrl, {
-        headers: {
-            Accept: 'application/gzip, text/csv',
-        },
-    });
+    const response = await fetch(csvUrl);
 
     if (!response.ok) {
         throw new Error(`Failed to fetch ${csvUrl} (${response.status})`);
     }
 
-    return await response.text();
+    const ds = new DecompressionStream('gzip');
+    const decompressedStream = response.body.pipeThrough(ds);
+    return await new Response(decompressedStream).text();
 }
